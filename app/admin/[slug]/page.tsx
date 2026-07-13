d125d56cfd8fdb4459654512a57841dbca743272
comunicacionatipico-code <comunicacionatipico@gmail.com>
2026-07-13 18:24:48 +0200

    Add noStore to admin page to prevent cache

--- app/admin/[slug]/page.tsx
+++ app/admin/[slug]/page.tsx
@@ -1,12 +1,15 @@
 import { notFound } from 'next/navigation'
+import { unstable_noStore as noStore } from 'next/cache'
 import { getRestauranteAsync } from '@/lib/restaurante'
 import AdminEditor from '@/components/AdminEditor'
 
 export const dynamic = 'force-dynamic'
+export const revalidate = 0
 
 interface Props { params: { slug: string } }
 
 export default async function AdminPage({ params }: Props) {
+  noStore()
   const data = await getRestauranteAsync(params.slug)
   if (!data) notFound()
   return <AdminEditor restaurante={data} slug={params.slug} />
